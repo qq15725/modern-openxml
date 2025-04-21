07c56dbed4c4529f5243d0b00bbbc6c00d219d12
wxm <157215725@qq.com>
2025-04-21 17:27:41 +0800

    fix: text layout

--- src/ooxml/presentation/text-body.ts
+++ src/ooxml/presentation/text-body.ts
@@ -52,6 +52,126 @@ export function parseTextBody(txBody?: OOXMLNode, ctx?: Record<string, any>): Te
   const placeholder = ctx?.placeholder
   const query = ctx?.query
 
+  const content = txBody?.get('.//a:p').map((p) => {
+    const lvl = p.attr<number>('a:pPr/@lvl', 'number') ?? 0
+    const lvKey = `a:lvl${lvl + 1}pPr`
+    const pPrList = [
+      () => p.find('a:pPr'),
+      () => p.find('../a:lstStyle/a:defPPr'),
+      () => p.find(`../a:lstStyle/${lvKey}`),
+      () => placeholder?.node?.find('p:txBody/a:lstStyle/a:defPPr'),
+      () => placeholder?.node?.find(`p:txBody/a:lstStyle/${lvKey}`),
+      () => placeholder && master?.node?.find(`p:txStyles/p:${placeholder.type}Style/${lvKey}`),
+      () => master?.node?.find(`p:txStyles/otherStyle/${lvKey}`),
+      () => theme?.node?.find(`p:objectDefaults/a:spDef/a:lstStyle/${lvKey}`),
+      () => theme?.node?.find(`p:objectDefaults/a:lnDef/a:lstStyle/${lvKey}`),
+      () => theme?.node?.find(`p:objectDefaults/a:txDef/a:lstStyle/${lvKey}`),
+      () => presentation?.node?.find(`p:defaultTextStyle/${lvKey}`),
+      () => presentation?.node?.find(`p:defaultTextStyle/a:defPPr`),
+      () => presentation?.node?.find(`p:defaultTextStyle/a:lvl1pPr`),
+    ]
+
+    const queryPPr = (path: string, type?: string): any => {
+      return pPrList.reduce((res, pPr) => res ?? pPr()?.query(path, type), undefined)
+    }
+
+    function parseStyle(r?: OOXMLNode): Record<string, any> {
+      const queryRPr = <T>(path: string, type?: OOXMLQueryType): T | undefined => {
+        return (
+          r?.query(`a:rPr/${path}`, type)
+          ?? queryPPr(`a:defRPr/${path}`, type)
+        ) as T
+      }
+      const fill = parseFill(queryRPr(fillXPath), ctx) as SolidFillDeclaration | undefined
+      const outline = parseOutline(queryRPr('a:ln'), ctx)
+
+      let fontFamily = queryRPr<string>('*[self::a:cs or self::a:ea or self::a:latin or self::a:sym]/@typeface', 'StringValue')
+      if (fontFamily) {
+        const fontScheme = ctx?.theme?.fontScheme
+        const type = fontFamily.startsWith('+mn-')
+          ? 'minor'
+          : fontFamily.startsWith('+mj-')
+            ? 'major'
+            : undefined
+        if (fontScheme && type) {
+          switch (fontFamily.substring(4)) {
+            case 'lt':
+              fontFamily = fontScheme[type]?.latin
+              break
+            case 'ea':
+              fontFamily = fontScheme[type]?.eastasian
+              break
+            case 'cs':
+              fontFamily = fontScheme[type]?.complexScript
+              break
+          }
+        }
+      }
+
+      return {
+        fontWeight: queryRPr('@b', 'boolean') ? 700 : undefined,
+        fontStyle: queryRPr('@i', 'boolean') ? 'italic' : undefined,
+        fontFamily,
+        textTransform: toTextTransform(queryRPr('@cap', 'string')),
+        textDecoration: toTextDecoration(queryRPr('@u', 'string')),
+        fontSize: queryRPr('@sz', 'fontSize'),
+        letterSpacing: queryRPr('@spc', 'fontSize'),
+        lineHeight: queryRPr('a:lnSpc/a:spcPct/@val', 'ST_TextSpacingPercentOrPercentString'),
+        color: fill?.color,
+        outline,
+      }
+    }
+
+    let hasRunOrBr = false
+    const fragments = p
+      .get('*')
+      .map((r) => {
+        switch (r.name) {
+          case 'a:r': {
+            hasRunOrBr = true
+            return {
+              ...parseStyle(r),
+              content: r.attr('a:t/text()', 'string')!,
+            }
+          }
+          case 'a:br':
+            hasRunOrBr = true
+            return { ...parseStyle(r), content: '\n' }
+          case 'a:endParaRPr':
+            return hasRunOrBr
+              ? undefined
+              : { ...parseStyle(r), content: '\n' }
+          default:
+            return undefined
+        }
+      })
+      .filter(Boolean) as FragmentContent[]
+
+    return {
+      marginLeft: queryPPr('@marL', 'emu'),
+      marginRight: queryPPr('@marR', 'emu'),
+      textIndent: queryPPr('@indent', 'emu'),
+      lineHeight: queryPPr('a:lnSpc/a:spcPct/@val', 'ST_TextSpacingPercentOrPercentString'),
+      textAlign: toTextAlign(queryPPr('@algn', 'string')),
+      fragments,
+    }
+  }) ?? []
+
+  const paragraphs = content
+  for (let i = 0; i < paragraphs.length; i++) {
+    const paragraph = paragraphs[i]
+    const fragments = paragraph.fragments
+    const lastFragment = fragments.length ? fragments[fragments.length - 1] : undefined
+    if (lastFragment?.content === '\n') {
+      if (fragments.length > 1) {
+        paragraphs.splice(i, 0, {
+          ...paragraph,
+          fragments: [fragments.pop()!],
+        })
+      }
+    }
+  }
+
   return {
     style: {
       paddingLeft: query('a:bodyPr/@lIns', 'ST_Coordinate32'),
@@ -68,113 +188,7 @@ export function parseTextBody(txBody?: OOXMLNode, ctx?: Record<string, any>): Te
       verticalAlign: toVerticalAlign(query('a:bodyPr/@anchor', 'string')),
     },
     text: {
-      content: txBody?.get('.//a:p').map((p) => {
-        const lvl = p.attr<number>('a:pPr/@lvl', 'number') ?? 0
-        const lvKey = `a:lvl${lvl + 1}pPr`
-        const pPrList = [
-          () => p.find('a:pPr'),
-          () => p.find('../a:lstStyle/a:defPPr'),
-          () => p.find(`../a:lstStyle/${lvKey}`),
-          () => placeholder?.node?.find('p:txBody/a:lstStyle/a:defPPr'),
-          () => placeholder?.node?.find(`p:txBody/a:lstStyle/${lvKey}`),
-          () => placeholder && master?.node?.find(`p:txStyles/p:${placeholder.type}Style/${lvKey}`),
-          () => master?.node?.find(`p:txStyles/otherStyle/${lvKey}`),
-          () => theme?.node?.find(`p:objectDefaults/a:spDef/a:lstStyle/${lvKey}`),
-          () => theme?.node?.find(`p:objectDefaults/a:lnDef/a:lstStyle/${lvKey}`),
-          () => theme?.node?.find(`p:objectDefaults/a:txDef/a:lstStyle/${lvKey}`),
-          () => presentation?.node?.find(`p:defaultTextStyle/${lvKey}`),
-          () => presentation?.node?.find(`p:defaultTextStyle/a:defPPr`),
-          () => presentation?.node?.find(`p:defaultTextStyle/a:lvl1pPr`),
-        ]
-
-        const queryPPr = (path: string, type?: string): any => {
-          return pPrList.reduce((res, pPr) => res ?? pPr()?.query(path, type), undefined)
-        }
-
-        function parseStyle(r?: OOXMLNode): Record<string, any> {
-          const queryRPr = <T>(path: string, type?: OOXMLQueryType): T | undefined => {
-            return (
-              r?.query(`a:rPr/${path}`, type)
-              ?? queryPPr(`a:defRPr/${path}`, type)
-            ) as T
-          }
-          const fill = parseFill(queryRPr(fillXPath), ctx) as SolidFillDeclaration | undefined
-          const outline = parseOutline(queryRPr('a:ln'), ctx)
-
-          let fontFamily = queryRPr<string>('*[self::a:cs or self::a:ea or self::a:latin or self::a:sym]/@typeface', 'StringValue')
-          if (fontFamily) {
-            const fontScheme = ctx?.theme?.fontScheme
-            const type = fontFamily.startsWith('+mn-')
-              ? 'minor'
-              : fontFamily.startsWith('+mj-')
-                ? 'major'
-                : undefined
-            if (fontScheme && type) {
-              switch (fontFamily.substring(4)) {
-                case 'lt':
-                  fontFamily = fontScheme[type]?.latin
-                  break
-                case 'ea':
-                  fontFamily = fontScheme[type]?.eastasian
-                  break
-                case 'cs':
-                  fontFamily = fontScheme[type]?.complexScript
-                  break
-              }
-            }
-          }
-
-          return {
-            fontWeight: queryRPr('@b', 'boolean') ? 700 : undefined,
-            fontStyle: queryRPr('@i', 'boolean') ? 'italic' : undefined,
-            fontFamily,
-            textTransform: toTextTransform(queryRPr('@cap', 'string')),
-            textDecoration: toTextDecoration(queryRPr('@u', 'string')),
-            fontSize: queryRPr('@sz', 'fontSize'),
-            letterSpacing: queryRPr('@spc', 'fontSize'),
-            lineHeight: queryRPr('a:lnSpc/a:spcPct/@val', 'ST_TextSpacingPercentOrPercentString'),
-            color: fill?.color,
-            outline,
-          }
-        }
-
-        let hasROrBr = false
-        const fragments = p
-          .get('*')
-          .map((r) => {
-            switch (r.name) {
-              case 'a:r': {
-                hasROrBr = true
-                return {
-                  ...parseStyle(r),
-                  content: r.attr('a:t/text()', 'string')!,
-                }
-              }
-              case 'a:br':
-                hasROrBr = true
-                return {
-                  ...parseStyle(r),
-                  content: '\n ', // TODO
-                }
-              case 'a:endParaRPr':
-                return hasROrBr
-                  ? undefined
-                  : { ...parseStyle(r), content: '\n ' } // TODO
-              default:
-                return undefined
-            }
-          })
-          .filter(Boolean) as FragmentContent[]
-
-        return {
-          marginLeft: queryPPr('@marL', 'emu'),
-          marginRight: queryPPr('@marR', 'emu'),
-          textIndent: queryPPr('@indent', 'emu'),
-          lineHeight: queryPPr('a:lnSpc/a:spcPct/@val', 'ST_TextSpacingPercentOrPercentString'),
-          textAlign: toTextAlign(queryPPr('@algn', 'string')),
-          fragments,
-        }
-      }) ?? [],
+      content,
     },
   }
 }
