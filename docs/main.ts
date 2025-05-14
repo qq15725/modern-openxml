fe64893753b1746307ad6f6af3083d5b8832d165
wxm <157215725@qq.com>
2025-05-14 18:32:06 +0800

    chore: playground

--- docs/main.ts
+++ docs/main.ts
@@ -1,31 +1,49 @@
 import presetShapeDefinitions from '../assets/presetShapeDefinitions'
-import { pptxToIDoc, pptxToSVG } from '../src'
+import { idocToPPTX, pptxToIDoc, pptxToSVG } from '../src'
 
-const input = document.createElement('input')
-document.body.appendChild(input)
-input.accept = '.pptx'
-input.type = 'file'
-input.onchange = async () => {
-  const file = input.files?.[0]
-  parsePPTX(new Uint8Array(await file!.arrayBuffer()))
+function openFileDialog(): Promise<Uint8Array> {
+  return new Promise((resolve) => {
+    const input = document.createElement('input')
+    input.type = 'file'
+    input.accept = '.pptx'
+    input.onchange = async () => {
+      const file = input.files?.[0]
+      resolve(new Uint8Array(await file!.arrayBuffer()))
+    }
+    input.click()
+  })
 }
 
-async function parsePPTX(source: Uint8Array): Promise<void> {
+const btn1 = document.querySelector('#decode') as HTMLButtonElement
+btn1.onclick = async () => {
+  testPPTXToSVG(await openFileDialog())
+}
+
+const btn2 = document.querySelector('#reEncode') as HTMLButtonElement
+btn2.onclick = async () => {
+  testPPTXReEncode(await openFileDialog())
+}
+
+async function testPPTXToSVG(source: Uint8Array): Promise<void> {
   console.warn(await pptxToIDoc(source, { presetShapeDefinitions }))
   const svg = await pptxToSVG(source, { presetShapeDefinitions })
   console.warn(svg)
   document.body.append(svg)
 }
 
-async function testPPTX(): Promise<void> {
-  for (const [key] of Object.entries(import.meta.glob('../test/fixtures/*.pptx', { query: '?raw' }))) {
-    if (!key.endsWith('complex2.pptx'))
-      continue
-    const filename = key.split('/').pop()
-    const source = await fetch(filename!).then(rep => rep.arrayBuffer())
-    parsePPTX(new Uint8Array(source))
-    console.warn(filename)
-  }
+async function testPPTXReEncode(source: Uint8Array): Promise<void> {
+  const doc = await pptxToIDoc(source, { presetShapeDefinitions })
+  const blob = new Blob([await idocToPPTX(doc)], { type: 'application/vnd.openxmlformats-officedocument.presentationml.presentation' })
+  downloadBlob(blob, 'output.pptx')
 }
 
-testPPTX()
+function downloadBlob(blob: Blob, filename: string): void {
+  const url = URL.createObjectURL(blob)
+  const link = document.createElement('a')
+  link.href = url
+  link.download = filename
+  document.body.appendChild(link)
+  link.click()
+  document.body.removeChild(link)
+  URL.revokeObjectURL(url)
+}
